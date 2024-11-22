da53e20f3334745224b4175024ada2c0f0cb75f6
SergeyGlazyrin <glazyrin_s@list.ru>
2024-11-22 19:29:28 +0300

    Готовая программа. В будущем дополнить автоматический поиск временной задержкой между бросками кубиков.

--- WinnerSearch/src/main/java/org/example/WinnerSearch.java
+++ WinnerSearch/src/main/java/org/example/WinnerSearch.java
@@ -6,41 +6,70 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
+import java.util.Map;
+import java.util.random.RandomGenerator;
 import javax.swing.*;
-import javax.swing.event.ListSelectionListener;
+
+/**
+ * Программа для поиска победителя розыгрыша. Список участников получает из файла в файловой системе.
+ * Победитель определяются методом бинарного поиска. Визуализация осуществленна с помощью пары игральных кубиков D6.
+ * Есть два варианта определения победителя. Автоматически и в ручную, эмуляцией каждого броска кубика.
+ *
+ * Примерный список участнико находится в папке static.
+ */
 
 public class WinnerSearch extends JPanel {
     private JList list;
     private DefaultListModel listModel;
+    private boolean stillSearching = true;
+    static final ImageIcon dice1 = new ImageIcon("WinnerSearch/src/static/1.png");
+    static final ImageIcon dice2 = new ImageIcon("WinnerSearch/src/static/2.png");
+    static final ImageIcon dice3 = new ImageIcon("WinnerSearch/src/static/3.png");
+    static final ImageIcon dice4 = new ImageIcon("WinnerSearch/src/static/4.png");
+    static final ImageIcon dice5 = new ImageIcon("WinnerSearch/src/static/5.png");
+    static final ImageIcon dice6 = new ImageIcon("WinnerSearch/src/static/6.png");
+    Map<Integer,ImageIcon> diceMap = Map.of(1,dice1,2,dice2,3,dice3,4,dice4,5,dice5,6,dice6);
+    JLabel firstDice = new JLabel(dice1);
+    JLabel secondDice = new JLabel(dice2);
 
     public WinnerSearch(){
-        super(new BorderLayout());
+        setLayout(new BorderLayout());
+        firstDice.setPreferredSize(new Dimension(200,200));
+        secondDice.setPreferredSize(new Dimension(200,200));
         listModel = new DefaultListModel();
         listModel.addElement("Выберите файл со списком участников");
         list = new JList(listModel);
         JScrollPane listScrollPane = new JScrollPane(list);
+        listScrollPane.setPreferredSize(new Dimension(400,700));
         add (new JLabel("Список участников:"), BorderLayout.NORTH);
-        add (listScrollPane);
+        add (listScrollPane, BorderLayout.WEST);
         add (new ButtonPanel(), BorderLayout.SOUTH);
+        add (firstDice,BorderLayout.CENTER);
+        add (secondDice, BorderLayout.EAST);
+
     }
 
 
     public class ButtonPanel extends JPanel {
 
-    JButton buttonWinner, chooseFile;
+    JButton buttonWinner, chooseFile, findWinnerManual;
     public String[] str;
 
     public ButtonPanel() {
-        buttonWinner = new JButton("Найти победителя");
+        buttonWinner = new JButton("Найти победителя автоматически");
+        findWinnerManual = new JButton("Найти победителя пошагово");
         chooseFile = new JButton("Выберите файл");
 
         var buttonStart = new ButtonStart();
         var buttonChooseFile = new ButtonChooseFile();
+        var buttonFindWinnerManual = new ButtonFindWinnerManual();
+
 
         buttonWinner.addActionListener(buttonStart);
         chooseFile.addActionListener(buttonChooseFile);
+        findWinnerManual.addActionListener(buttonFindWinnerManual);
 
+        add(findWinnerManual);
         add(buttonWinner);
         add(chooseFile);
     }
@@ -48,7 +77,54 @@ public class WinnerSearch extends JPanel {
     public class ButtonStart implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
+            int size = listModel.getSize();
 
+                while (size > 1) {
+                    int firstDiceRoll = RandomGenerator.getDefault().nextInt(1, 7);
+                    int secondDiceRoll = RandomGenerator.getDefault().nextInt(1, 7);
+                    firstDice.setIcon(diceMap.get(firstDiceRoll));
+                    secondDice.setIcon(diceMap.get(secondDiceRoll));
+                    if (firstDiceRoll != secondDiceRoll) {
+                        if (firstDiceRoll > secondDiceRoll) {
+                            listModel.removeRange((int) Math.floor(size / 2), size - 1);
+                        } else {
+                            listModel.removeRange(0, (int) Math.floor(size / 2)-1);
+                        }
+                    }
+                    size = listModel.getSize();
+
+                }
+                listModel.addElement("");
+                listModel.addElement("Победитель: " + listModel.getElementAt(0) + ". Поздравляем!!!");
+
+        }
+    }
+
+    public class ButtonFindWinnerManual implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int size = listModel.getSize();
+            int sizeToDelete = (int) Math.floor(listModel.getSize() / 2);
+
+            if (stillSearching) {
+                if (size > 1) {
+                    int firstDiceRoll = RandomGenerator.getDefault().nextInt(1, 7);
+                    int secondDiceRoll = RandomGenerator.getDefault().nextInt(1, 7);
+                    firstDice.setIcon(diceMap.get(firstDiceRoll));
+                    secondDice.setIcon(diceMap.get(secondDiceRoll));
+                    if (firstDiceRoll != secondDiceRoll) {
+                        if (firstDiceRoll > secondDiceRoll) {
+                            listModel.removeRange(size - (int) Math.floor(listModel.getSize() / 2), size - 1);
+                        } else {
+                            listModel.removeRange(0, (int) Math.floor(listModel.getSize() / 2)-1);
+                        }
+                    }
+                } else {
+                    listModel.addElement("");
+                    listModel.addElement("Победитель: " + listModel.getElementAt(0) + ". Поздравляем!!!");
+                    stillSearching = false;
+                }
+            }
         }
     }
 
@@ -87,7 +163,6 @@ public class WinnerSearch extends JPanel {
         JComponent contentPane = new WinnerSearch();
         contentPane.setOpaque(true);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(600,600);
         frame.setContentPane(contentPane);
         frame.pack();
         frame.setVisible(true);
--- WinnerSearch/src/test/java/ButtonPanelTest.java
+++ WinnerSearch/src/test/java/ButtonPanelTest.java
@@ -8,12 +8,17 @@ import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Тест для первой версии программы. Предназначен для проверки корректной чтения файлаи заполнения массива.
+ *
+ * В данный момент не рабочий.
+ */
 public class ButtonPanelTest {
-    Path path = Path.of("C:\\Users\\Михаил\\IdeaProjects\\CrptApi\\WinnerSearchGit\\WinnerSearch\\src\\static\\участники.txt");
+    Path path = Path.of("C:\\Users\\Михаил\\IdeaProjects\\CrptApi\\WinnerSearchGit\\WinnerSearch\\src\\static\\участникиТест.txt");
     List<String> strTest = List.of("Дмитрий", "Sergey Krivchikov", "Rodion Akhmetov");
     @Test
     void getArrayOfParTest(){
-        ButtonPanel bp = new ButtonPanel();
+        WinnerSearch.ButtonPanel bp = new ButtonPanel();
         var bcf = bp.new ButtonChooseFile();
         List<String> str = Arrays.asList(bcf.getArrayOfPar(path));
         assertLinesMatch(strTest,str);
